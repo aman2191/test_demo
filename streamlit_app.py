48c18ab5adf60f42a8a8c48eb284ef21b6152663
Aman Farkade <afarkade@in.pepper-advantage.com>
2025-04-26 15:40:46 +0530

    Initial commit4

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,10 @@ import base64
 import traceback
 from datetime import datetime
 import requests
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementClickInterceptedException
